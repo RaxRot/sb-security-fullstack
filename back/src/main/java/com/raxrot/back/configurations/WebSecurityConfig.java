377c1a441513542d4203b1b33a8be9c4f55d67a4
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-07-23 20:58:11 +0100

    configure multiple users in memory

--- back/src/main/java/com/raxrot/back/configurations/WebSecurityConfig.java
+++ back/src/main/java/com/raxrot/back/configurations/WebSecurityConfig.java
@@ -6,6 +6,9 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -15,12 +18,30 @@ public class WebSecurityConfig
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         http.authorizeHttpRequests(requests ->{
-           requests.requestMatchers("/hello","/public/**").permitAll().anyRequest().authenticated();
+           requests.anyRequest().authenticated();
         });
         http.sessionManagement(session ->
                 session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         http.httpBasic(Customizer.withDefaults());
         http.csrf(csrf->csrf.disable());
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        if (!manager.userExists("user")){
+            manager.createUser(User.withUsername("user")
+                    .password("{noop}12345")
+                    .roles("USER")
+                    .build());
+        }
+        if (!manager.userExists("admin")){
+            manager.createUser(User.withUsername("admin")
+                    .password("{noop}12345")
+                    .roles("ADMIN")
+                    .build());
+        }
+        return manager;
     }
 }
