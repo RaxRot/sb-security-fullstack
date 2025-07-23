6f5124f3030fa8fe3703b4ceff15aec9844fc612
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-07-23 20:03:09 +0100

    add NoteServiceImpl

---
+++ back/src/main/java/com/raxrot/back/services/impl/NoteServiceImpl.java
@@ -0,0 +1,60 @@
+package com.raxrot.back.services.impl;
+
+import com.raxrot.back.exceptions.ApiException;
+import com.raxrot.back.models.Note;
+import com.raxrot.back.repositories.NoteRepository;
+import com.raxrot.back.services.NoteService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class NoteServiceImpl implements NoteService {
+    private final NoteRepository noteRepository;
+    public NoteServiceImpl(NoteRepository noteRepository) {
+        this.noteRepository = noteRepository;
+    }
+
+    @Override
+    public Note createNoteForUser(String username, String content) {
+        Note note = new Note();
+        note.setContent(content);
+        note.setOwnerUsername(username);
+        Note savedNote = noteRepository.save(note);
+        return savedNote;
+    }
+
+    @Override
+    public List<Note> getNotesForUser(String username) {
+        List<Note> personalNotes = noteRepository
+                .findByOwnerUsername(username);
+        return personalNotes;
+    }
+
+    @Override
+    public Note updateNoteForUser(Long noteId, String content, String username) {
+        Note note = noteRepository.findById(noteId).orElseThrow(()
+                -> new ApiException("Note not found"));
+
+        //temp protection
+        if (!note.getOwnerUsername().equals(username)) {
+            throw new ApiException("You cannot edit someone else's note");
+        }
+
+        note.setContent(content);
+        Note updatedNote = noteRepository.save(note);
+        return updatedNote;
+    }
+
+    @Override
+    public void deleteNoteForUser(Long noteId, String username) {
+        Note note = noteRepository.findById(noteId)
+                .orElseThrow(() -> new ApiException("Note not found"));
+
+        //temp protection
+        if (!note.getOwnerUsername().equals(username)) {
+            throw new ApiException("You cannot delete someone else's note");
+        }
+        noteRepository.delete(note);
+    }
+}
