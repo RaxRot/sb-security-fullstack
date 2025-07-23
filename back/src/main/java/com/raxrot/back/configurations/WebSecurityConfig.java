ca4bcb88fd8cdaa8f41948f6c7ae451e5dd482d0
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-07-23 17:55:50 +0100

    add basic WebSecurityConfig

---
+++ back/src/main/java/com/raxrot/back/configurations/WebSecurityConfig.java
@@ -0,0 +1,26 @@
+package com.raxrot.back.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig
+{
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests(requests ->{
+           requests.requestMatchers("/hello").permitAll().anyRequest().authenticated();
+        });
+        http.sessionManagement(session ->
+                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.httpBasic(Customizer.withDefaults());
+        http.csrf(csrf->csrf.disable());
+        return http.build();
+    }
+}
