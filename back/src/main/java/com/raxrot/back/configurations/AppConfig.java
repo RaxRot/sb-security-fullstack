46c1ccfac92a03e35188ccb23ede5aed5aaa2192
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-07-23 21:07:22 +0100

    add constants to make code more clear

--- back/src/main/java/com/raxrot/back/configurations/AppConfig.java
+++ back/src/main/java/com/raxrot/back/configurations/AppConfig.java
@@ -1,4 +1,7 @@
 package com.raxrot.back.configurations;
 
 public class AppConfig {
+    public static final String NOTE_ERROR_NOT_FOUND = "Note not found";
+    public static final String NOTE_ERROR_DELETE_FORBIDDEN = "You cannot delete someone else's note";
+    public static final String NOTE_ERROR_UPDATE_FORBIDDEN = "You cannot edit someone else's note";
 }
