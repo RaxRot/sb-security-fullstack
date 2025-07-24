01bfbb1c13ce4295a18637b8a44d1c5440d4db51
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-07-24 14:56:53 +0100

    add CommandLineRunner to test creation roles and users

--- back/src/main/java/com/raxrot/back/configurations/WebSecurityConfig.java
+++ back/src/main/java/com/raxrot/back/configurations/WebSecurityConfig.java
@@ -1,17 +1,20 @@
 package com.raxrot.back.configurations;
 
+import com.raxrot.back.models.AppRole;
+import com.raxrot.back.models.Role;
+import com.raxrot.back.models.User;
+import com.raxrot.back.repositories.RoleRepository;
+import com.raxrot.back.repositories.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-import javax.sql.DataSource;
+import java.time.LocalDate;
 
 @Configuration
 @EnableWebSecurity
@@ -29,6 +32,7 @@ public class WebSecurityConfig
         return http.build();
     }
 
+    /*
     @Bean
     public UserDetailsService userDetailsService(DataSource dataSource) {
         JdbcUserDetailsManager manager = new JdbcUserDetailsManager(dataSource);
@@ -45,5 +49,45 @@ public class WebSecurityConfig
                     .build());
         }
         return manager;
+    }
+     */
+
+    @Bean
+    public CommandLineRunner initData(RoleRepository roleRepository, UserRepository userRepository) {
+        return args -> {
+            Role userRole = roleRepository.findByRoleName(AppRole.ROLE_USER)
+                    .orElseGet(() -> roleRepository.save(new Role(AppRole.ROLE_USER)));
+
+            Role adminRole = roleRepository.findByRoleName(AppRole.ROLE_ADMIN)
+                    .orElseGet(() -> roleRepository.save(new Role(AppRole.ROLE_ADMIN)));
+
+            if (!userRepository.existsByUserName("user")) {
+                User user1 = new User("user", "user@example.com", "{noop}12345");
+                user1.setAccountNonLocked(false);
+                user1.setAccountNonExpired(true);
+                user1.setCredentialsNonExpired(true);
+                user1.setEnabled(true);
+                user1.setCredentialsExpiryDate(LocalDate.now().plusYears(1));
+                user1.setAccountExpiryDate(LocalDate.now().plusYears(1));
+                user1.setTwoFactorEnabled(false);
+                user1.setSignUpMethod("email");
+                user1.setRole(userRole);
+                userRepository.save(user1);
+            }
+
+            if (!userRepository.existsByUserName("admin")) {
+                User admin = new User("admin", "admin@example.com", "{noop}12345");
+                admin.setAccountNonLocked(true);
+                admin.setAccountNonExpired(true);
+                admin.setCredentialsNonExpired(true);
+                admin.setEnabled(true);
+                admin.setCredentialsExpiryDate(LocalDate.now().plusYears(1));
+                admin.setAccountExpiryDate(LocalDate.now().plusYears(1));
+                admin.setTwoFactorEnabled(false);
+                admin.setSignUpMethod("email");
+                admin.setRole(adminRole);
+                userRepository.save(admin);
+            }
+        };
     }
 }
