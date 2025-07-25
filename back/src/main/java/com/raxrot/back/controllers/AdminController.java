544eb74a01e8eb67c27d8fdb79bf30273698f3eb
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-07-25 14:56:39 +0100

    add @PreAuthorize("hasRole('ROLE_ADMIN')")

--- back/src/main/java/com/raxrot/back/controllers/AdminController.java
+++ back/src/main/java/com/raxrot/back/controllers/AdminController.java
@@ -5,10 +5,12 @@ import com.raxrot.back.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@PreAuthorize("hasRole('ROLE_ADMIN')")
 @RestController
 @RequestMapping("/api/admin")
 public class AdminController {
